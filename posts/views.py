580e22ef4a3a032750db1447074ef1ffaf344722
Shohagh019 <shohaghmunna@gmail.com>
2024-11-25 00:16:27 +0600

    comment section added

--- posts/views.py
+++ posts/views.py
@@ -42,8 +42,27 @@ class DeletePost(DeleteView):
 
 class PostDetails(DetailView):
     model = models.Post
-    form_class = forms.postForm
+    # form_class = forms.postForm
     template_name = 'post_details.html'
-    success_url = reverse_lazy('profile')
+    # success_url = reverse_lazy('profile')
     pk_url_kwarg = 'id' 
+    def post(self, request, *args, **kwargs):
+        comment_form = forms.CommentForm(data=self.request.POST)
+        post = self.get_object()
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+        return self.get(request, *args, **kwargs)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = self.object # post model er object ekhane store korlam
+        comments = post.comments.all()
+        comment_form = forms.CommentForm()
+        
+        context['comments'] = comments
+        context['comment_form'] = comment_form
+        return context       
+    
    
